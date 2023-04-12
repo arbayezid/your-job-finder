8e5f6e08076c2385b9c01cba8c19c20622b50720
arbayezid <arbayezid0@gmail.com>
2023-04-12 20:12:46 +0600

    Career Hub Project Submit

--- src/main.jsx
+++ src/main.jsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <Home></Home>,
-        loader: () => fetch('featureList.json')
+        loader: () => fetch('/featureList.json')
       },
       {
         path: '/:id',
